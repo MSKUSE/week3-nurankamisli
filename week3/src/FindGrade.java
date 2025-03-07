007e27ceca333cd97c0a1e1caf94a549010c1fc0
nurankamisli <kamislinuran4@gmail.com>
2025-03-07 12:07:24 +0300

    week3-nurankamisli

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -2,6 +2,37 @@ public class FindGrade{
 
     public static void main(String[] args){
 
+        if(args.length>=1){
+        
+            int score=Integer.parseInt(args[0]);
+
+         
+
+            if (score>=90){
+                System.out.println("Grade: A");
+
+            }
+            else if (score>=80 && score<89){
+                System.out.println("Grade: B");
+            }
+            else if(score>=70 && score<79){
+                System.out.println("Grade: C");
+            }
+            else if(score>=60 && score <69){
+                System.out.println("Grade: D");
+            }
+            else if (score>=0 && score<59){
+                System.out.println("Grade: F");
+            }
+
+        }else{
+            System.out.println("Please provide a score");
+        }
+        
+
+
+
+
         //Get the score from the user as a file parameter
         //Calculate the grade based on the score
         //Print the grade to the console
